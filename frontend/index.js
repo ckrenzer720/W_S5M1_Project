a694e8abef9c6edc380768c3367a59afc82f3cd2
Conner Krenzer <krenzer84@gmail.com>
2024-07-16 13:54:56 -0400

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -7,15 +7,60 @@ function moduleProject1() {
 
   // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
   //  ✨ add your code here
+  const widgetTag = document.querySelectorAll('section div');
+  widgetTag.forEach(widget => widget.classList.add('widget'));
 
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
+  const randomIdx = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIdx];
+  const quoteDiv = document.createElement('div');
+  const quoteText = randomQuote.quote;
+  quoteDiv.textContent = quoteText;
+  const widget1 = document.querySelector(".quoteoftheday");
+  widget1.appendChild(quoteDiv);
+
+  const authorDate = document.createElement('div');
+  const { author, date } = randomQuote;
+  authorDate.textContent = `${author} in ${date || "an unknown date"}`;
+  widget1.appendChild(authorDate);
 
   // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
+  const randomVerb1 = Math.floor(Math.random() * verbs.length);
+  const randomVerb2 = Math.floor(Math.random() * verbs.length);
+
+  const randomAdverb1 = Math.floor(Math.random() * adverbs.length);
+  const randomAdverb2 = Math.floor(Math.random() * adverbs.length);
+
+  const randomNoun1 = Math.floor(Math.random() * nouns.length);
+  const randomNoun2 = Math.floor(Math.random() * nouns.length);
+
+  const corporateSpeak = ` We need to ${verbs[randomVerb1]} our ${nouns[randomNoun1]} ${adverbs[randomAdverb1]}
+    in order to ${verbs[randomVerb2]} our ${nouns[randomNoun2]} ${adverbs[randomAdverb2]}.`;
+    // console.log(corporateSpeak);
+
+  const speak = document.createElement('p');
+  speak.textContent = corporateSpeak;
+  const corporateWidget = document.querySelector('.corporatespeak');
+  corporateWidget.appendChild(speak);
 
   // 👉 TASK 4 - Build a "Countdown" widget
   //  ✨ add your code here
+  const countdownWidget = document.querySelector('.countdown');
+  const countdownElement = document.createElement('p');
+  let count = 5;
+  countdownElement.textContent = `T-minus ${count}...`
+  countdownWidget.appendChild(countdownElement);
+  const id = setInterval(() => {
+    if (count === 1) {
+      countdownElement.textContent = "Liftoff! 🚀"
+      clearInterval(id)
+    } else {
+      countdownElement.textContent = `T-minus ${--count}...`
+    }
+  }, 1000)
+
 
   // 👉 TASK 5 - Build a "Friends" widget
   //  ✨ add your code here
